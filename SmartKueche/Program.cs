1d01cceacf1348fa61c3f157db9f2d1e51a807b4
Arber Alaj <Arber.Alaj@ksb-sg.ch>
2019-10-02 12:25:16 +0200

    Add project files.

---
+++ SmartKueche/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartKueche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
